e1fda61b777c3d0264dce840557db92048654c77
User0939 <>
2022-12-07 11:04:20 +0530

    header and lottie animation

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, TouchableOpacity, Dimensions ,Image, ScrollView} from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity, Dimensions, Image, ScrollView } from 'react-native'
 import React from 'react'
 import { useNavigation } from "@react-navigation/native";
 import GetImage from '../assets/GetImage';
@@ -6,14 +6,14 @@ import GetImage from '../assets/GetImage';
 export default function Header(props) {
     const height = Dimensions.get("screen").height;
     const navigation = useNavigation();
-   const{title,onPress,righticon}=props
+    const { title, onPress, righticon } = props
     return (
-        <View style={{flex:1,backgroundColor:"#ffff"}}> 
+        <View style={{ flex: 1, backgroundColor: "#ffff" }}>
             <View
                 style={styles.container}
             >
                 <TouchableOpacity
-                    onPress={() => props.navigation.goBack(null)  }
+                    onPress={() => props.navigation.goBack(null)}
                     style={styles.sub_Con}
                 >
                     <Image
@@ -27,29 +27,36 @@ export default function Header(props) {
                             fontSize: (height / 100) * 2.2,
                             fontFamily: "Urbanist_semibold",
                             fontWeight: 'bold',
-                            color:"#000"
-                            
+                            color: "#000"
+
                         }}
                     >
-                       {title}
+                        {title}
                     </Text>
                 </View>
-                <TouchableOpacity onPress={onPress} style={{flex:0.1,justifyContent:"center",alignItems:'center'}}>
-                <Image
-                        source={GetImage.choose}
+                {righticon &&
+                    <TouchableOpacity
+                        onPress={onPress}
                         style={{
-                            height:15,
-                            width:15,
-                            resizeMode:'contain'
-                        }}
-                    />
-            </TouchableOpacity>
+                            flex: 0.1,
+                            justifyContent: "center",
+                            alignItems: 'center'
+                        }}>
+                        <Image
+                            source={GetImage.choose}
+                            style={{
+                                height: 15,
+                                width: 15,
+                                resizeMode: 'contain'
+                            }}
+                        />
+                    </TouchableOpacity>}
             </View>
-            <View style={righticon?styles.margin:styles.sub_scroll}>
-              
+            <View style={righticon ? styles.margin : styles.sub_scroll}>
+
                 {props.children}
-            
-               
+
+
             </View>
         </View>
     )
@@ -71,14 +78,14 @@ const styles = StyleSheet.create({
         width: 18,
         resizeMode: "contain"
     },
-    margin:{
-        flex:0.95,
-        margin:15
+    margin: {
+        flex: 0.95,
+        margin: 15
     },
-    sub_scroll:{
-        flex:0.95,
-       
-        
+    sub_scroll: {
+        flex: 0.95,
+
+
     }
 
 })
